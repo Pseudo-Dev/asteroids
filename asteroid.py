947fd3413618a9b1140a593a7377febc87ba4629
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-12-14 01:36:28 +0200

    Clean up fluff

--- asteroid.py
+++ asteroid.py
@@ -10,11 +10,10 @@ class Asteroid:
     def __init__(self):
         self.dX = randint(-50, 50)
         self.dY = randint(-50, 50)
-        self.speed = int(sqrt(self.dX**2 + self.dY**2))
         self.size = randint(50, 200)
-        self.color = color[randint(0, 7)]
         loc = Point(200 + randint(0, 3600), 200 + randint(0, 2600))
         self.circle = Circle(loc, self.size)
+        self.color = color[randint(0, 7)]
         self.circle.setFill(self.color)
         if self.color != "yellow" and self.color != "white":
             self.circle.setOutline(self.color)
