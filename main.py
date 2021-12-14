f930b58e10765bcd95795cc472a09ca65c682fdb
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-12-14 02:06:03 +0200

    Continuous animation

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ win = GraphWin("Node 1", screenW, screenH, autoflush=False)
 win.setCoords(0, 0, screenW*10, screenH*10)
 list = []
 
-for i in range(6):
-    asteroid = Asteroid()
-    list.append(asteroid)
-    asteroid.circle.draw(win)
-
 while True:
+    if len(list) < 7:
+        new = Asteroid()
+        list.append(new)
+        new.circle.draw(win)
+
     for asteroid in list:
         asteroid.circle.move(asteroid.dX, asteroid.dY)
+        x = asteroid.circle.getCenter().getX()
+        y = asteroid.circle.getCenter().getY()
+        if x <= 0 or y <=0 or x >= screenW * 10 or y >= screenH * 10:
+            asteroid.circle.undraw()
+            list.remove(asteroid)
 
     if win.checkMouse():
         break
