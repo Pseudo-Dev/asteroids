ca10b272d9cc02bff1ed4e3c8b49bcdec045d7cd
Petri Riihikallio <petri.riihikallio@metis.fi>
2021-12-22 16:10:07 +0200

    Simplify direction calculation

--- app/direction.py
+++ app/direction.py
@@ -1,43 +1,36 @@
-from remote.client import peerDict
-
-
-gridW = 1
-gridH = 3
+gridWidth = 2
+gridHeight = 3
 
 
 def up(node):
     result = []
     while node > 0:
-        node -= gridW
-        if node in peerDict:
-            result.append(node)
+        node -= gridWidth
+        result.append(node)
     return result
 
 
 def down(node):
     result = []
-    while node <= gridW * gridH:
-        node += gridW
-        if node in peerDict:
-            result.append(node)
+    while node <= gridWidth * gridHeight:
+        node += gridWidth
+        result.append(node)
     return result
 
 
 def left(node):
     result = []
-    edge = int((node-1) / gridW) * gridW
+    edge = int((node-1) / gridWidth) * gridWidth
     while node > edge:
         node -= 1
-        if node in peerDict:
-            result.append(node)
+        result.append(node)
     return result
 
 
 def right(node):
     result = []
-    edge = int((node-1) / gridW + 1) * gridW
+    edge = int((node-1) / gridWidth + 1) * gridWidth
     while node <= edge:
         node += 1
-        if node in peerDict:
-            result.append(node)
+        result.append(node)
     return result
